80e0aaed6470d17b63615b9d4c818543541cec5a
tonyyalvess2017-creator <tonyyalvess2017@gmail.com>
2026-08-26 22:34:29 -0300

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -4,36 +4,58 @@ const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-    cors: { origin: "*" }
-});
+const io = new Server(server, { cors: { origin: "*" } });
 
-// Serve o arquivo HTML do jogo direto da raiz
 app.use(express.static(__dirname));
 
 let players = {};
+let foods = [];
+
+// Gera comida inicial na arena gigante
+for (let i = 0; i < 1000; i++) {
+    foods.push({
+        id: i,
+        x: Math.random() * 5000,
+        y: Math.random() * 5000,
+        color: ['#ff0055', '#ffe600', '#00f0ff', '#a855f7', '#00ff66'][Math.floor(Math.random() * 5)]
+    });
+}
 
 io.on('connection', (socket) => {
     console.log(`[+] Jogador conectado: ${socket.id}`);
 
-    // Recebe a posição do cliente
-    socket.on('player_sync', (data) => {
-        players[socket.id] = data;
+    socket.on('join_game', (data) => {
+        players[socket.id] = {
+            id: socket.id,
+            x: data.x,
+            y: data.y,
+            angle: 0,
+            score: 30,
+            body: data.body,
+            color: data.color
+        };
     });
 
-    // Remove o jogador ao desconectar
+    socket.on('player_update', (data) => {
+        if (players[socket.id]) {
+            players[socket.id].x = data.x;
+            players[socket.id].y = data.y;
+            players[socket.id].angle = data.angle;
+            players[socket.id].score = data.score;
+            players[socket.id].body = data.body;
+        }
+    });
+
     socket.on('disconnect', () => {
         console.log(`[-] Jogador desconectado: ${socket.id}`);
         delete players[socket.id];
     });
 });
 
-// Envia a lista atualizada para TODOS os clientes conectados a cada 30 milissegundos
+// Loop de sincronização global da arena
 setInterval(() => {
-    io.emit('atualizar_arena', players);
-}, 1000 / 30);
+    io.emit('server_tick', { players, foods });
+}, 1000 / 60);
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-});
+server.listen(PORT, () => console.log(`Engine rodando na porta ${PORT}`));
