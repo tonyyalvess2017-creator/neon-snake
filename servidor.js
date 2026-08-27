bd25c096a0a19f38ea30e5e2da2bbf32c41144ef
tonyyalvess2017-creator <tonyyalvess2017@gmail.com>
2026-08-26 22:26:26 -0300

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -1,43 +1,36 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
+    cors: { origin: "*" }
 });
 
-// Faz o servidor carregar o index.html e os arquivos da pasta automaticamente
-app.use(express.static(path.join(__dirname)));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-let jogadores = {};
+let players = {};
 
 io.on('connection', (socket) => {
-    console.log(`> Novo jogador conectado: ${socket.id}`);
+    console.log(`[+] Jogador conectado: ${socket.id}`);
 
-    socket.on('player_sync', (dados) => {
-        dados.id = socket.id;
-        jogadores[socket.id] = dados;
-        io.emit('atualizar_arena', jogadores);
+    // Recebe a sincronização da cobra do jogador
+    socket.on('player_sync', (data) => {
+        players[socket.id] = data;
     });
 
+    // Desconexão
     socket.on('disconnect', () => {
-        console.log(`> Jogador desconectado: ${socket.id}`);
-        delete jogadores[socket.id];
-        io.emit('atualizar_arena', jogadores);
+        console.log(`[-] Jogador desconectado: ${socket.id}`);
+        delete players[socket.id];
     });
 });
+
+// Envia o estado da arena para todos a cada frame de rede (30 FPS)
+setInterval(() => {
+    io.emit('atualizar_arena', players);
+}, 1000 / 30);
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`> Servidor Neon Snake rodando na porta ${PORT}`);
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
